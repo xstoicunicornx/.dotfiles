6f71a0168e9da05979eedc09b2e4f2d729e0e5bf
xstoicunicornx <xstoicunicornx@users.noreply.github.com>
2026-05-24 02:05:47 -0500

    nvim: adjust colors to align with tokyonight moon theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -21,29 +21,30 @@ return {
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
-      vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#3b4261' })
+      vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#8b93c4' })
       vim.api.nvim_set_hl(0, 'LineNr', { fg = '#545c7e' })
-      vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#3b4261' })
+      vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#8b93c4' })
       vim.api.nvim_set_hl(0, 'DiagnosticUnnecessary', { fg = '#737aa2' })
-      vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#2a4556' })
-      vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#4b2a3d' })
-      vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#4a4530' })
+      vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#3f4e30' })
+      vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#52252c' })
+      vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#2e4268' })
       vim.api.nvim_set_hl(0, 'DiffText', { bg = '#394b70' })
-      vim.api.nvim_set_hl(0, 'Comment', { fg = '#636da6' })
+      vim.api.nvim_set_hl(0, 'Comment', { fg = '#8b93c4' })
+      vim.api.nvim_set_hl(0, 'Visual', { bg = '#7a5faa' })
     end,
   },
-  {
-    'catppuccin/nvim',
-    enabled = false,
-    lazy = false,
-    name = 'catppuccin',
-    priority = 1000,
-
-    config = function()
-      require('catppuccin').setup {
-        transparent_background = true,
-      }
-      vim.cmd.colorscheme 'catppuccin-frappe'
-    end,
-  },
+  -- {
+  --   'catppuccin/nvim',
+  --   enabled = false,
+  --   lazy = false,
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --
+  --   config = function()
+  --     require('catppuccin').setup {
+  --       transparent_background = true,
+  --     }
+  --     vim.cmd.colorscheme 'catppuccin-frappe'
+  --   end,
+  -- },
 }
